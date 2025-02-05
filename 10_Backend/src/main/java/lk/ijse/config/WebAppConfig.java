bba695cedde28ce561a74c5101767f930f9b8a7f
Lahiru Chanaka <kodikaragelahiru@gmail.com>
2025-02-05 16:07:02 +0530

    Spring lecture Day 03

---
+++ 10_Backend/src/main/java/lk/ijse/config/WebAppConfig.java
@@ -0,0 +1,31 @@
+package lk.ijse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.controller")
+@EnableWebMvc
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(("/WEB-INF/views/**")).addResourceLocations("/WEB-INF/views/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");  // use created file prefix folder name (WEB-INF/views in this case)
+        resolver.setSuffix(".html");    // use created file suffix (.html in this case)
+        resolver.setOrder(2);   // To give priority to this resolver. If not given, it will be the last resolver.
+        return resolver;
+    }
+}
---
+++ 10_Backend/src/main/java/lk/ijse/controller/CustomerController.java
@@ -0,0 +1,54 @@
+package lk.ijse.controller;
+
+import lk.ijse.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/customer")
+public class CustomerController {
+
+    private List<CustomerDTO> customers = new ArrayList<>();
+
+    @PostMapping(path = "save")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        customers.add(customerDTO);
+        return customerDTO;
+    }
+
+    @GetMapping(path = "getAll")
+    public List<CustomerDTO> getCustomer() {
+        return customers;
+    }
+
+    @PutMapping(path = "update")
+    public CustomerDTO updateCustomer(@RequestBody CustomerDTO customerDTO) {
+
+        for (int i = 0; i < customers.size(); i++) {
+
+            CustomerDTO existingCustomer = customers.get(i);
+
+            if (existingCustomer.getId().equals(customerDTO.getId())) {
+                existingCustomer.setName(customerDTO.getName());
+                existingCustomer.setAddress(customerDTO.getAddress());
+                existingCustomer.setAge(customerDTO.getAge());
+                return customerDTO;
+            }
+        }
+        return customerDTO;
+    }
+
+    @DeleteMapping(path = "delete/{id}")
+    public boolean deleteCustomer(@PathVariable(value = "id") String id) {
+        for (int i = 0; i < customers.size(); i++) {
+            CustomerDTO existingCustomer = customers.get(i);
+            if (existingCustomer.getId().equals(id)) {
+                customers.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
