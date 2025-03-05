ec154f1ca7b0580a45f77a649415286876580db7
Lahiru Chanaka <kodikaragelahiru@gmail.com>
2025-03-05 12:09:54 +0530

    Spring Boot lecture Day 04

--- 16_Spring_Boot_Loggins/src/main/java/lk/ijse/controller/DemoController.java
+++ 16_Spring_Boot_Loggins/src/main/java/lk/ijse/controller/DemoController.java
@@ -1,18 +1,28 @@
 package lk.ijse.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("api/v1/demo")
 public class DemoController {
 
+    // Trace > Debug > Info > Warn > Error
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
     @GetMapping("/log-demo")
     public String logDemo(){
+        log.trace("Trace Message");
+        log.debug("Debug Message");
+        log.info("Info Message");
+        log.warn("Warn Message");
+        log.error("Error Message");
 
-        return "Log Demo Called";
+        return "Logging Demo Called...";
     }
-
 
 }
